182fe97c9ee4a85f053558264654a5311f0fdabe
Suchintan Singh <suchintan@hearsaycorp.com>
2015-09-04 18:13:13 +0000

    Added gitconfig to dotfiles

--- install.sh
+++ install.sh
@@ -5,11 +5,13 @@ echo Deleting existing files
 rm ~/.bashrc
 rm ~/.vimrc
 rm ~/.tmux.conf
+rm ~/.gitconfig
 
-CURRENT_DIR=$(pwd) 
+CURRENT_DIR=$(pwd)
 
 echo Porting new ones
 
 ln -s $CURRENT_DIR/bash/.bashrc ~/.bashrc
 ln -s $CURRENT_DIR/tmux/.tmux.conf ~/.tmux.conf
-ln -s $CURRENT_DIR/vim/.vimrc ~/.vimrc 
+ln -s $CURRENT_DIR/vim/.vimrc ~/.vimrc
+ln -s $CURRENT_DIR/git/.gitconfig ~/.gitconfig
